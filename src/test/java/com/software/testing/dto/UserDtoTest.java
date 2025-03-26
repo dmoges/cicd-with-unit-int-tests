0555c19d0de0e5c584d7e6d0e8f718f5f264ed56
Dawit Moges <dmogesde@gmx.net>
2025-03-26 10:41:39 +0100

    a unit test added to for a dto

---
+++ src/test/java/com/software/testing/dto/UserDtoTest.java
@@ -0,0 +1,16 @@
+package com.software.testing.dto;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+
+@JsonTest
+public class UserDtoTest {
+
+    @Test
+    void myFirstTest() {
+      assertThat(1).isEqualTo(42);
+    }
+
+}
